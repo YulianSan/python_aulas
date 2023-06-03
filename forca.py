91472d2ac7fb1efb4f63793c5064c0b71d22d372
YulianSan <knoob0733@gmail.com>
2023-06-02 22:21:11 -0300

    Commit initial para o game forca

---
+++ forca.py
@@ -0,0 +1,11 @@
+def forca():
+    palavras = ['cavalo', 'aguia', 'gato', 'vaca']
+    
+    ROW = '************************************'
+    print(ROW)
+    print('Jogo da forca')
+    print(ROW)
+    
+
+if __name__ == '__main__':
+    forca()
