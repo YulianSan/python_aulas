e6efe2a91783b8bc06be4d042069190a90052cb5
YulianSan <knoob0733@gmail.com>
2023-06-02 22:22:33 -0300

    Um commit inicial para o menu

---
+++ menu.py
@@ -0,0 +1,8 @@
+ROW = '************************************'
+print(ROW)
+print('Escolha o game')
+print(ROW)
+print('1 - adivinhe')
+print('2 - forca')
+game = input('Escolha o jogo')
+print(f'Você escolheu {game}')
